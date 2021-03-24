e82f15d3708e205fac3ad713dcd43f7d08504a88
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-03-24 19:18:32 -0300

    Adiciona resolução do exercício 2

--- bloco_4/dia_1/exercises.js
+++ bloco_4/dia_1/exercises.js
@@ -13,3 +13,17 @@ console.log(subtraction);
 console.log(multiplication);
 console.log(division);
 console.log(mod);
+
+
+//Exercício 2.
+
+let c = 5;
+let d = 8;
+
+if (c > d) {
+  console.log(c)
+} else if (c < d){
+  console.log(d)
+} else {
+  console.log(c, "e", d, "são iguais")
+}
