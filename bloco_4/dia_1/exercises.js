d62efb30967e7dff2c2098b09da93a9bc3b34fb8
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-03-24 19:13:50 -0300

    Exercício 1

---
+++ bloco_4/dia_1/exercises.js
@@ -0,0 +1,15 @@
+// Exercício 1.
+let a = 15;
+let b = 10;
+
+addition = a + b;
+subtraction = a-b;
+multiplication = a * b;
+division = a / b;
+mod = a % 2;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(mod);
