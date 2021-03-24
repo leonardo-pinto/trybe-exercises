76136933b1191db98da61bdff2508c6bb295c521
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-03-24 20:23:36 -0300

    Adiciona resolução do exercício 7

--- bloco_4/dia_1/exercises.js
+++ bloco_4/dia_1/exercises.js
@@ -92,4 +92,20 @@ switch (piece){
     console.log("error")
 }
 
+// Exercício 7.
 
+let grade = 75;
+
+if (grade >= 90) {
+  console.log ("A"); 
+} else if (grade >= 80 & grade < 90) {
+  console.log ("B");
+} else if (grade >= 70 && grade < 80) {
+  console.log ("C");
+} else if (grade >= 60 && grade < 70) {
+  console.log ("D");
+} else if (grade >= 50 && grade < 60) {
+  console.log ("E");
+} else {
+  console.log ("F");
+}
