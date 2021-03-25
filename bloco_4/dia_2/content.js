f8c27709ada5b98fb9fb8e89bd0a923e7d2a4ad9
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-03-25 15:39:40 -0300

    Adiciona a resolução dos exercícios do conteúdo 'For' e 'For of'

--- bloco_4/dia_2/content.js
+++ bloco_4/dia_2/content.js
@@ -1,3 +1,5 @@
+// Conteúdo Arrays
+
 let menu = ['Home','Serviços','Portfólio','Links'];
 let menuServices = menu [1];
 
@@ -10,3 +12,34 @@ console.log(indexOfPortfolio);
 menu.push('Contato');
 
 console.log(menu);
+
+// Conteúdo For
+
+let groceryList = ['Arroz','Feijão','Alface','Melancia'];
+
+for (let index = 0; index < groceryList.length; index +=1) {
+  console.log(groceryList[index]);
+}
+
+// Conteúdo For of
+
+let numeros = [1,2,3,4,5,6];
+for (let numero of numeros) {
+  console.log(numero)
+}
+
+let word = 'Xablau';
+for (let letter of word) {
+  console.log(letter);
+}
+
+let arrOfNumbers = [10,20,30];
+for (let sum of arrOfNumbers) {
+  sum += 1;
+  console.log(sum);
+}
+
+let names = ['João','Maria','Antônio','Margarida'];
+for (let nameList of names) {
+  console.log(nameList);
+}
