9ab82633bd29bb6e8c9fd85dfa1178cc783d0cfd
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-03-25 18:39:27 -0300

    Resolução do exercício 2

--- bloco_4/dia_2/exercises.js
+++ bloco_4/dia_2/exercises.js
@@ -5,3 +5,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 }
+
+// Exercício 2.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for (index = 0; index < numbers.length; index += 1) {
+  sum = sum + numbers[index];
+}
+console.log(sum);
