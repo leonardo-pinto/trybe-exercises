7963b935d22c30d83f298acb19d840d357263055
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-03-25 18:43:15 -0300

    Adiciona resolução do exercício 4

--- bloco_4/dia_2/exercises.js
+++ bloco_4/dia_2/exercises.js
@@ -18,3 +18,22 @@ console.log(sum);
 // Exercício 3.
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for (index = 0; index < numbers.length; index += 1) {
+  sum = sum + numbers[index];
+}
+console.log(sum/numbers.length);
+
+// Exercício 4.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for (index = 0; index < numbers.length; index += 1) {
+  sum = sum + numbers[index];
+}
+
+if (sum/numbers.length > 20) {
+  console.log("Valor maior que 20");
+} else {
+  console.log("Valor menor ou igual a 20");
+}
