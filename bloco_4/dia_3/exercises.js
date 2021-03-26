b1fce86a84905fd2df16604a2401f01fb39c986b
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-03-26 16:21:08 -0300

    Resolução do exercício bônus 6

--- bloco_4/dia_3/exercises.js
+++ bloco_4/dia_3/exercises.js
@@ -33,3 +33,29 @@ for (let index = 0; index <= n; index += 1) {
   }
 }
 
+// Exercício 5
+
+let n = 5;
+
+for (let index = 0; index <= n; index += 1) {
+  if (index % 2 != 0) {
+    console.log(' '.repeat((n-index)/2), '*'.repeat(index), ' '.repeat((n-index)/2));
+  }
+}
+
+// Exercício 6
+
+let n = 20;
+let numberOfDiv = 0;
+
+for (let index = 1; index <= n; index += 1) {
+  if (n % index === 0) {
+    numberOfDiv += 1;
+  }
+}
+
+if (numberOfDiv <= 3) {
+  console.log(n, "é um número primo !!!");
+} else {
+  console.log(n, "não é um número primo :(");
+}
