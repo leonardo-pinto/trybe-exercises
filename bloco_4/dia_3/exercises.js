4bbe4676249770956c0919a63afea6febc54cb7a
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-03-26 15:47:11 -0300

    Resolução do exercício 3

--- bloco_4/dia_3/exercises.js
+++ bloco_4/dia_3/exercises.js
@@ -13,3 +13,13 @@ let n = 5;
 for (let index = 0; index <= n; index += 1) {
   console.log('*'.repeat(index));
 }
+
+
+// Exercício 3
+
+let n = 5;
+
+for (let index = 0; index <= n; index += 1) {
+  console.log(' '.repeat(n-index), '*'.repeat(index));
+}
+
