f8d81d63aae7baa1362f70bbd9b28e495e25e8cc
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-03-29 15:29:31 -0300

    Conteúdo sobre For/In

--- bloco_4/dia_4/content.js
+++ bloco_4/dia_4/content.js
@@ -1,3 +1,5 @@
+// Objetos
+
 let player = {
   name: 'Marta',
   lastName: 'Silva',
@@ -20,4 +22,26 @@ console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e '
 
 console.table(player);
 
+// For/in
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+} 
+
+for (let index in names) {
+  console.log('Olá ' + names[index]);
+};
+
+
+let carro = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+}
+
+for (let index in carro) {
+  console.log(index,carro[index]);
+};
 
