82ac9e9b856644e0311b9fac135f5037660878f4
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-04-28 19:23:36 -0300

    Parte 2: exercício 1

--- bloco_7/dia_3/script.js
+++ bloco_7/dia_3/script.js
@@ -110,3 +110,23 @@ const obj3 = {
 
 //Compare dois objetos (JSON) para verificar se são idênticos ou não
 assert.deepStrictEqual(obj1, obj3);
+///////////////////////////////////////////
+
+
+const assert = require('assert');
+const addOne = (arr) => {
+  let output = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    output.push(arr[index] + 1);
+  }
+  return output;
+}
+
+const myArray = [31, 57, 12, 5];
+const unchanged = [31, 57, 12, 5];
+const expected = [32, 58, 13, 6];
+const output = addOne(myArray);
+
+assert.strictEqual(typeof addOne, 'function');
+assert.deepStrictEqual(output, expected);
+assert.deepStrictEqual(myArray, unchanged);
