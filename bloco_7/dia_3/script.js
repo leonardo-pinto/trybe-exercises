ace6cc46d7db595c5fa213ff7fef042290f23099
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-04-28 19:27:37 -0300

    Parte 2: exercício 2

--- bloco_7/dia_3/script.js
+++ bloco_7/dia_3/script.js
@@ -130,3 +130,21 @@ const output = addOne(myArray);
 assert.strictEqual(typeof addOne, 'function');
 assert.deepStrictEqual(output, expected);
 assert.deepStrictEqual(myArray, unchanged);
+///////////////////////////////////////////////////
+
+
+const assert = require('assert');
+const wordLengths = (arr) => {
+  let output = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    output.push(arr[index].length);
+  }
+  return output;
+}
+
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
+assert.deepStrictEqual(output, expected);
