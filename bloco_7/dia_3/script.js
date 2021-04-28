d3998bbd659c651765fc7a0ede40d513614b0270
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-04-28 19:06:11 -0300

    Parte 1: exercício 2

--- bloco_7/dia_3/script.js
+++ bloco_7/dia_3/script.js
@@ -23,3 +23,27 @@ function sum(a, b) {
 /* assert.throws(() => {
   sum(4,'5');
 }, /^Error: parameters must be numbers$/); */
+
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+//Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+//assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+
+//Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+//assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+//Verifique se o array passado por parâmetro não sofreu alterações
+
+
+//Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+//assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4])
