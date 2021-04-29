c1184d20a6f6bd5576983cd1761f50348e634a9d
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-04-29 09:11:33 -0300

    Exercícios Parte 3

--- bloco_7/dia_3/script.js
+++ bloco_7/dia_3/script.js
@@ -195,3 +195,81 @@ words = ['plant', 'shelf', 'arrow', 'bird'];
 expected = -1;
 output = findTheNeedle(words, 'plat');
 assert.strictEqual(output, expected);
+
+/////////////////////////////////////////////////////////
+const greetPeople = (people) => {
+  let greeting = [];
+  for (const person in people) {
+    greeting.push(`Hello ${people[person]}`);
+  }
+  return greeting;
+};
+
+//Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
+
+const assert = require('assert');
+const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+assert.deepStrictEqual(greetPeople(parameter), result);
+//////////////////////////////////////////////////////////////////////////
+
+//Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
+
+const removeVowels = (word) => {
+  const characters = word.split('');
+  const results = [];
+  let counter = 0;
+  for (let index = 0; index < characters.length; index += 1) {
+    if (
+      characters[index] === 'a' ||
+      characters[index] === 'o' ||
+      characters[index] === 'i' ||
+      characters[index] === 'e' ||
+      characters[index] === 'u'
+    ) {
+      counter += 1;
+      results.push(counter);
+    } else {
+      results.push(characters[index]);
+    }
+  }
+  return results.join('');
+};
+
+const assert = require('assert');
+const parameter = 'Dayane';
+const result = 'D1y2n3';
+assert.deepStrictEqual(removeVowels(parameter), result);
+
+/////////////////////////////////////////////////////////////////
+
+const greaterThanTen = (array) => {
+  let results = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > 10) {
+      results.push(array[index]);
+    }
+  }
+  return results;
+};
+
+const parameter = [4, 10, 32, 9, 21];
+const result = [32, 21];
+const assert = require('assert');
+assert.deepStrictEqual(greaterThanTen(parameter), result);
+//////////////////////////////////////////////////////////////////
+
+
+function secondThirdSmallest(array) {
+  let results = []
+  array.sort((x,y) => {
+    return x - y;
+  });
+  results.push(array[1], array[2]);
+  return results;
+};
+
+const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
+const result = [5, 6];
+const assert = require('assert');
+assert.deepStrictEqual(secondThirdSmallest(parameter), result);
