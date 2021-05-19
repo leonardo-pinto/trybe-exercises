6cc01200783072da1c0556086657d816d1fe06fa
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-05-19 18:18:35 -0300

    Exercício 3

--- bloco_10/dia_2/exercise2-3.test.js
+++ bloco_10/dia_2/exercise2-3.test.js
@@ -23,6 +23,36 @@ describe('getUserName as promise', () => {
 });
 
 
+describe('getUserName as async/await', () => {
+  describe('when the user id exists', () => {
+    it('id 4 should return Mark', async () => {
+      expect.assertions(1);
+      const data = await getUserName(4);
+      expect(data).toEqual('Mark')
+    });
+
+
+
+    it('id 5 should return Paul', async () => {
+      expect.assertions(1);
+      const data = await getUserName(5);
+      expect(data).toEqual('Paul')
+    });
+  });
+
+  describe('when the user id does not exists', () => {
+    it('should return an error', async () => {
+      expect.assertions(1);
+      try {
+        await getUserName(1);
+      } catch (error) {
+        expect(error).toEqual({ error: 'User with 1 not found.' })
+      }
+    });
+  });
+});
+
+
 
 
   
