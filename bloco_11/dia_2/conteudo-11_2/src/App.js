5420365dc01472d9af46395c3a43598d958c51fc
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-05-25 10:15:21 -0300

    Conteúdo sobre Props

---
+++ bloco_11/dia_2/conteudo-11_2/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import Image from './Image';
+
+class App extends React.Component {
+  render() {
+    return <Image source='cat-pic' alternativeText='Cute cat staring'/>
+  }
+}
+
+export default App;
