9f3c3818e9d77255d28f1b2fb5ca6f9f2f0fbfcb
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-27 15:42:48 -0300

    ex2

---
+++ bloco_26/dia_2/exercises/exercise2.js
@@ -0,0 +1,22 @@
+function calculate(num1, num2, num3) {
+  return new Promise((resolve, reject) => {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof num3 !== 'number') {
+      reject(new Error("Informe apenas números"));
+    }
+
+    const resultado = ((num1 + num2) * num3);
+    if (resultado < 50) reject(new Error("Valor muito baixo"))
+    if (resultado >= 50) resolve(resultado);
+  });
+}
+
+function randomNumber () {
+  return Math.floor(Math.random() * 100 + 1);
+};
+
+const generateRandomNumbers = Array.from({ length: 3 }).map(randomNumber);
+
+calculate(...generateRandomNumbers)
+  .then(res => console.log(res))
+  .catch(err => console.log(err.message));
+  
