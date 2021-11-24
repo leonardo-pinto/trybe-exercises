9411ea47897370f28ad4cfeabc8770b3292e6727
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-11-24 15:00:54 -0300

    altera exemplo com requisições do tipo eager e lazy loading

--- bloco_29/dia_3/relation-1-n/index.js
+++ bloco_29/dia_3/relation-1-n/index.js
@@ -4,6 +4,47 @@ const { Address, Employee } = require('./models');
 
 const app = express();
 
+
+// exemplo de requisição utilizando o "Eager Loading"
+// app.get('/employees/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const employee = await Employee.findOne({
+//       where: { id },
+//       include: [{ 
+//         model: Address, as: 'addresses', attributes: { exclude: ['number'] }, 
+//       }],
+//     });
+
+//     if (!employee) return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+//     return res.status(200).json(employee);
+//   } catch (e) {
+//     console.log(e.message);
+//     return res.status(500).json({ message: 'Algo deu errado' });
+//   }
+// })
+
+// exemplo de requisição utilizando o "Lazy Loading"
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({ where: { id } });
+
+    if (!employee) return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    if (req.query.includeAddresses === 'true') {
+      const addresses = await Address.findAll({ where: { employeeId: id } });
+      return res.status(200).json({ employee, addresses });
+    }
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+})
+
 app.get('/employees', async (_req, res) => {
   try {
     const employees = await Employee.findAll({
