db1993a873d1eda553a6d88a999fd84852720271
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-11-30 14:45:08 -0300

    exercises/29.3

--- bloco_29/dia_3/relation-n-n/index.js
+++ bloco_29/dia_3/relation-n-n/index.js
@@ -23,7 +23,37 @@ app.get('/usersbooks/:id', async (req, res) => {
     console.log(e.message);
     return res.status(500).json({ message: 'Algo deu errado' });
   }
-})
+});
+
+app.post('/createuser', async (req, res) => {
+  try {
+    const { firstName, lastName, age } = req.body;
+  
+    const newUser = await User.create({ firstName, lastName, age });
+
+
+    return res.status(200).json(newUser);
+
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+app.post('/createbook', async (req, res) => {
+  try {
+    const { name, releaseYear, numberPages } = req.body;
+  
+    const newBook = await Book.create({ name, releaseYear, numberPages });
+
+
+    return res.status(200).json(newBook);
+
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
